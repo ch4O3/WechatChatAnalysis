6bacdcecf4442f3fe6aaa2f311cc6ea81863cdb5
suyu.zhu@tuya.com <suyu.zhu@tuya.com>
2022-01-07 15:22:59 +0800

    摸鱼

--- main.go
+++ main.go
@@ -37,8 +37,7 @@ func (ms MapSorter) Len() int {
 }
 
 func (ms MapSorter) Less(i, j int) bool {
-	return ms[i].Val > ms[j].Val // 按值排序
-	//return ms[i].Key < ms[j].Key // 按键排序
+	return ms[i].Val > ms[j].Val
 }
 
 func (ms MapSorter) Swap(i, j int) {
@@ -63,6 +62,19 @@ func main() {
 	scanner := bufio.NewScanner(f)
 	//初始化userCount，用于存放群聊的用户名及发言次数。
 	userCount := make(map[string]int)
+	//初始化content集合
+	contentCi := 0
+
+	//图片数量
+	Piccount := 0
+
+	//开发语言
+	gocount := 0
+	javacount := 0
+	netcount := 0
+	phpcoumt := 0
+	jsconut := 0
+	pythoncount := 0
 
 	for scanner.Scan() {
 		patternStr := `^.{1,10}\s[(][2][0][2][0-2]-[0-9][0-9]-[0-9][0-9]\s[0-9][0-9]:[0-9][0-9]:[0-9][0-9][)]:.*`
@@ -73,6 +85,7 @@ func main() {
 			t.username = a[0]
 			t.time = b[0]
 			t.content = b[1]
+			contentCi++
 			//群聊用户出现次数
 			v, ok := userCount[t.username]
 			if ok {
@@ -80,6 +93,29 @@ func main() {
 			} else {
 				userCount[t.username] = 1
 			}
+			//群聊聊过的内容,分词
+			if strings.Contains(t.content, "[图片]") {
+				Piccount++
+			}
+			if strings.Contains(t.content, "go") {
+				gocount++
+			}
+			if strings.Contains(t.content, "java") {
+				javacount++
+			}
+			if strings.Contains(t.content, ".net") {
+				netcount++
+			}
+			if strings.Contains(t.content, "php") {
+				phpcoumt++
+			}
+			if strings.Contains(t.content, "js") {
+				jsconut++
+			}
+			if strings.Contains(t.content, "python") {
+				pythoncount++
+			}
+
 		}
 	}
 	//统计群聊中发言Top10并输出
@@ -93,5 +129,25 @@ func main() {
 		if count == 10 {
 			break
 		}
+	}
+	//图片统计
+	fmt.Printf("聊天记录存在 %v张 图片\n\n", Piccount)
+
+	//发言统计
+	fmt.Printf("摸鱼摸了%v条\n\n", contentCi)
+
+	//开发语言讨论分析
+	language := map[string]int{
+		"Go":         gocount,
+		"Java":       javacount,
+		".Net":       netcount,
+		"Php":        phpcoumt,
+		"Javascript": jsconut,
+		"Python":     pythoncount,
+	}
+	ab := NewMapSorter(language)
+	sort.Sort(ab)
+	for _, item := range ab {
+		fmt.Printf("%v语言讨论%d次\n", item.Key, item.Val)
 	}
 }
